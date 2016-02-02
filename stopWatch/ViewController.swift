b2347794c6f318ac06a7700971dd80b21e14027e
ReemAlattas <ralattas@live.com>
2016-02-02 15:15:56 -0500

    First Commit

--- stopWatch/ViewController.swift
+++ stopWatch/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var timer = NSTimer()
+    
+    var count = 0
+    
+    func result() {
+        count++
+        countLabel.text = "\(count)"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
